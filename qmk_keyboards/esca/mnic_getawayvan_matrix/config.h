ff758bcd7bbe435690775e12237a8cc4bb78bf9f
Marshall Nichols <marshall.nichols@duke.edu>
2022-05-25 09:01:44 -0400

    Add permissive hold for getawayvan

--- qmk_keyboards/esca/mnic_getawayvan_matrix/config.h
+++ qmk_keyboards/esca/mnic_getawayvan_matrix/config.h
@@ -19,6 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
+/* Custom Additions */
+#define PERMISSIVE_HOLD
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xE5CA
 #define PRODUCT_ID      0x0401
--- qmk_keyboards/esca/mnic_getawayvan_matrix/keymaps/default/keymap.c
+++ qmk_keyboards/esca/mnic_getawayvan_matrix/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC,       KC_Q,    KC_W,    KC_E,  KC_R, KC_T,    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_T(KC_TAB), KC_A,    KC_S,    KC_D,  KC_F, KC_G,    KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT,       KC_Z,    KC_X,    KC_C,  KC_V, KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
-    MO(5),       KC_ALT, KC_LGUI, LT(3, KC_SPC),        LT(2, KC_SPC), KC_RGUI,  KC_LEFT, KC_DOWN,    KC_RIGHT
+    MO(5),       KC_LALT, KC_LGUI, LT(3, KC_SPC),        LT(2, KC_SPC), KC_RGUI,  KC_LEFT, KC_DOWN,    KC_RIGHT
   ),
   [1] = LAYOUT( /* Colemak-DH */
     KC_GESC,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN,  KC_BSPC,

aeb4b3b1f31480bfcdc6e5712b1ce1f9e57d8c69
Marshall Nichols <marshall.nichols@duke.edu>
2022-05-25 09:33:26 -0400

    Slightly shorten tapping term for getawayvan

--- qmk_keyboards/esca/mnic_getawayvan_matrix/config.h
+++ qmk_keyboards/esca/mnic_getawayvan_matrix/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Custom Additions */
 #define PERMISSIVE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 120
 
 
 /* USB Device descriptor parameter */
